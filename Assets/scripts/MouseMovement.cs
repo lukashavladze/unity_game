1cbf057960fa1be1e91906681677c3f66e5efa68
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2025-10-24 17:41:02 +0400

    added draganddrop, inventory, and item slots

--- Assets/scripts/MouseMovement.cs
+++ Assets/scripts/MouseMovement.cs
@@ -12,26 +12,29 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void Start()
     {
+        //locking the cursor to the middle of the screen and making it invisible
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!InventorySystem.Instance.isOpen) 
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // control rotation around x axis (look up and down)
-        xRotation -= mouseY;
+            // control rotation around x axis (look up and down)
+            xRotation -= mouseY;
 
-        //we clamp the rotation so we cant over-rotate (like in real life)
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            //we clamp the rotation so we cant over-rotate (like in real life)
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        // control rotation around y axis (loop up and down)
-        yRotation += mouseX;
+            // control rotation around y axis (loop up and down)
+            yRotation += mouseX;
 
-        //applying both rotations
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-
+            //applying both rotations
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        }
     }
 
 
